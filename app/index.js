50e168122519faaa1f9103800dc60e5adc0c715b
“Shaun <shaunwmusic@gmail.com>
2017-03-03 22:05:50 -0600

    About Me Done

--- app/index.js
+++ app/index.js
@@ -1,1 +1,23 @@
 import 'whatwg-fetch';
+
+function toggleBody() {
+  document.getElementById('displayAbout').classList.toggle('visible');
+  document.getElementById('displayContact').classList.toggle('visible');
+}
+
+function displayModal() {
+  document.getElementById('modal').classList.add('modal-visible');
+}
+
+function hideModal() {
+  document.getElementById('modal').classList.remove('modal-visible');
+}
+
+const navlistener = document.getElementById('nav');
+navlistener.addEventListener('click', toggleBody);
+
+const btnlistener = document.getElementById('button');
+btnlistener.addEventListener('click', displayModal);
+
+const timeslistener = document.getElementById('fa-times');
+timeslistener.addEventListener('click', hideModal);
